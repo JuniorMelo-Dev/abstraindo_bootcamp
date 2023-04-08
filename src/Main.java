bca96820d4aa99e07e6208d2062a3b42472bed57
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-04-07 23:10:19 -0300

    Implementando testes nas classes de dominio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -19,13 +21,42 @@ public class Main {
 //      Conteudo conteudo = new Conteudo(); //erro por ser classe abstract
 //      Conteudo conteudo = new Curso(); //posso criar Curso, pois é filha de Conteudo()
 
-        Mentoria mentoria1 = new Mentoria();
-        mentoria1.setTitulo("Curso Java");
-        mentoria1.setDescricao("Descrevendo Mentoria do Curso de Java");
-        mentoria1.setData(LocalDate.now());
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Curso Java");
+        mentoria.setDescricao("Descrevendo Mentoria do Curso de Java");
+        mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria1);
+        */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Banco PAN Java Developer");
+        bootcamp.setDescricao("Insira a descrição do Bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJunior = new Dev();
+        devJunior.setNome("Junior");
+        devJunior.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos Junior" + devJunior.getConteudosInscritos());
+        devJunior.progredir();
+        System.out.println("Conteudo Inscritos Junior" + devJunior.getConteudosInscritos());
+        System.out.println("Conteudo Concluidos Junior" + devJunior.getConteudosConcluidos());
+        System.out.println("XP: " + devJunior.calcularTotalXp());
+
+        System.out.println("---------");
+
+        Dev devLuana = new Dev();
+        devLuana.setNome("Luana");
+        devLuana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos Luana" + devLuana.getConteudosInscritos());
+        devLuana.progredir();
+        System.out.println("Conteudo Inscritos Luana" + devLuana.getConteudosInscritos());
+        System.out.println("Conteudo Concluidos Luana" + devLuana.getConteudosConcluidos());
+        System.out.println("XP: " + devLuana.calcularTotalXp());
     }
 }
